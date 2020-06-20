31264390afc6a4649c9049904a96ae93cf477948
grze <debuguj@gmail.com>
2020-06-20 19:33:14 +0200

    Update tests for ArchivedSpot.

--- src/test/java/pl/debuguj/system/spot/ArchivedSpotTest.java
+++ src/test/java/pl/debuguj/system/spot/ArchivedSpotTest.java
@@ -2,6 +2,8 @@ package pl.debuguj.system.spot;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.util.SerializationUtils;
 
 import javax.validation.Validation;
@@ -10,33 +12,17 @@ import javax.validation.ValidatorFactory;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Currency;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Optional;
+import java.time.DateTimeException;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ArchivedSpotTest {
 
-    private final SimpleDateFormat simpleDateTimeFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-    private final String registrationNumber = "WZE12345";
-    private Date startDate;
-    private Date finishDate;
-    private Date incorrectFinishDate;
     private CurrencyRate currencyRate = new CurrencyRate(new BigDecimal(1.0), Currency.getInstance(new Locale("pl", "PL")));
     private Validator validator;
-
-    public ArchivedSpotTest() {
-        try {
-            startDate = simpleDateTimeFormatter.parse("2017-10-12T10:15:10");
-            finishDate = simpleDateTimeFormatter.parse("2017-10-12T15:15:10");
-            incorrectFinishDate = simpleDateTimeFormatter.parse("2017-10-10T15:15:10");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
 
     @BeforeEach
     public void setup() {
@@ -46,7 +32,9 @@ class ArchivedSpotTest {
 
     @Test
     public void shouldBeSerializable() {
-        ArchivedSpot archivedSpot = new ArchivedSpot(registrationNumber, DriverType.REGULAR, startDate, finishDate);
+        final Date[] startStopTimestamps = createStartStopTimestamps();
+
+        final ArchivedSpot archivedSpot = new ArchivedSpot("WZE12345", DriverType.REGULAR, startStopTimestamps[0], startStopTimestamps[1]);
         ArchivedSpot other = (ArchivedSpot) SerializationUtils.deserialize(SerializationUtils.serialize(archivedSpot));
 
         assertThat(other.getUuid()).isEqualTo(archivedSpot.getUuid());
@@ -57,76 +45,98 @@ class ArchivedSpotTest {
     }
 
     @Test
-    public void shouldBeSetCorrectParameters() {
-        ArchivedSpot archivedSpot = new ArchivedSpot(registrationNumber, DriverType.REGULAR, startDate, finishDate);
+    public void shouldAcceptCorrectParameters() {
+        final Date[] startStopTimestamps = createStartStopTimestamps();
+        ArchivedSpot archivedSpot = new ArchivedSpot("WZE12345", DriverType.REGULAR, startStopTimestamps[0], startStopTimestamps[1]);
 
-        assertThat(archivedSpot.getVehiclePlate()).isEqualTo(registrationNumber);
+        assertThat(archivedSpot.getVehiclePlate()).isEqualTo("WZE12345");
         assertThat(archivedSpot.getDriverType()).isEqualTo(DriverType.REGULAR);
         assertThat(archivedSpot.getDriverType()).isNotEqualTo(DriverType.VIP);
-        assertThat(archivedSpot.getBeginDate()).isEqualTo(startDate);
-        assertThat(archivedSpot.getFinishDate()).isEqualTo(finishDate);
+        assertThat(archivedSpot.getBeginDate()).isEqualTo(startStopTimestamps[0]);
+        assertThat(archivedSpot.getFinishDate()).isEqualTo(startStopTimestamps[1]);
     }
 
     @Test
     public void shouldReturnEmptyOptionalBecauseOfNullFinishDate() {
-        ArchivedSpot archivedSpot = new ArchivedSpot(registrationNumber, DriverType.REGULAR, startDate, null);
+        final ArchivedSpot archivedSpot = new ArchivedSpot("WZE12345", DriverType.REGULAR, new Date(), null);
 
         assertEquals(Optional.empty(), archivedSpot.getFee());
     }
 
     @Test
-    public void shouldReturnEmptyOptionalBecauseOfIncorrectFinishDate() {
-        ArchivedSpot archivedSpot = new ArchivedSpot(registrationNumber, DriverType.REGULAR, startDate, incorrectFinishDate);
+    public void shouldThrowExceptionBecauseFinishDateIsBeforeStartDate() {
+        final Date[] incorrectStartStopTimestamps = createIncorrectStartStopTimeStamps();
 
-        assertEquals(Optional.empty(), archivedSpot.getFee());
+        final Spot spot = new Spot("WZE12345", DriverType.REGULAR, incorrectStartStopTimestamps[0]);
+        Exception exception = assertThrows(DateTimeException.class, () ->
+                new ArchivedSpot(spot, incorrectStartStopTimestamps[1]));
+
+        assertEquals("Finish date is to late", exception.getMessage());
+
     }
 
-    @Test
-    public void shouldReturnCorrectFeeForRegularDriver() throws Exception {
-        String[] startDates = {"2017-10-12T11:15:48", "2017-10-12T11:15:48",
-                "2017-10-12T11:15:48", "2017-10-12T11:15:48",
-                "2017-10-12T00:15:48", "2017-10-12T11:15:48", "2020-12-12T10:10:10"
-        };
-        String[] finishDates = {"2017-10-12T11:35:12", "2017-10-12T12:35:12",
-                "2017-10-12T13:35:12", "2017-10-12T16:35:12",
-                "2017-10-12T15:35:12", "2017-10-13T11:14:12", "2020-12-12T22:13:10"
-        };
-        BigDecimal[] returnedFee = {new BigDecimal("1.0"), new BigDecimal("3.0"),
-                new BigDecimal("7.0"), new BigDecimal("63.0"),
-                new BigDecimal("65535.0"), new BigDecimal("16777215.0"), new BigDecimal("8191.0")
-        };
+    @ParameterizedTest
+    @CsvSource({"2020-06-12T11:15:48, 2020-06-12T11:35:12, 1.0",
+            "2020-06-12T11:15:48, 2020-06-12T12:35:12, 3.0",
+            "2020-06-12T11:15:48, 2020-06-12T13:35:12, 7.0",
+            "2020-06-12T11:15:48, 2020-06-12T16:35:12, 63.0",
+            "2020-06-12T00:15:48, 2020-06-12T15:35:12, 65535.0",
+            "2020-06-12T11:15:48, 2020-06-13T11:14:12, 16777215.0",
+            "2020-06-12T10:10:10, 2020-06-12T22:13:10, 8191.0"
+    })
+    public void shouldReturnCorrectFeeForRegularDriver(String startTime, String stopTime, String fee) throws Exception {
 
-        for (int i = 0; i < startDates.length; i++) {
-            Date start = simpleDateTimeFormatter.parse(startDates[i]);
-            Date stop = simpleDateTimeFormatter.parse(finishDates[i]);
-            ArchivedSpot archivedSpot = new ArchivedSpot(registrationNumber, DriverType.REGULAR, start, stop);
+        final Date[] startStopTimestamps = createStartStopTimestampsByDates(startTime, stopTime);
+        final BigDecimal feeValue = new BigDecimal(fee);
 
-            assertEquals(returnedFee[i], archivedSpot.getFee(currencyRate).get());
-        }
+        ArchivedSpot archivedSpot = new ArchivedSpot("WZE12345", DriverType.REGULAR, startStopTimestamps[0], startStopTimestamps[1]);
+
+        assertEquals(feeValue, archivedSpot.getFee(currencyRate).get());
     }
 
-    @Test
-    public void shouldReturnCorrectFeeForVipDriver() throws Exception {
-        String[] beginDates = {"2017-10-12T11:15:48", "2017-10-12T11:15:48",
-                "2017-10-12T11:15:48", "2017-10-12T11:15:48",
-                "2017-10-12T00:15:48", "2017-10-12T11:15:48"
-        };
-        String[] endDates = {"2017-10-12T11:35:12", "2017-10-12T12:35:12",
-                "2017-10-12T13:35:12", "2017-10-12T16:35:12",
-                "2017-10-12T15:35:12", "2017-10-13T11:14:12"
-        };
-        BigDecimal[] returnedFee = {new BigDecimal("0.0"), new BigDecimal("2.0"),
-                new BigDecimal("5.0"), new BigDecimal("26.4"),
-                new BigDecimal("1747.6"), new BigDecimal("44887.0")
-        };
+    @ParameterizedTest
+    @CsvSource({"2020-10-12T11:15:48, 2020-10-12T11:35:12, 0.0",
+            "2020-10-12T11:15:48, 2020-10-12T12:35:12, 2.0",
+            "2020-10-12T11:15:48, 2020-10-12T13:35:12, 5.0",
+            "2020-10-12T11:15:48, 2020-10-12T16:35:12, 26.4",
+            "2020-10-12T00:15:48, 2020-10-12T15:35:12, 1747.6",
+            "2020-10-12T11:15:48, 2020-10-13T11:14:12, 44887.0"
+    })
+    public void shouldReturnCorrectFeeForVipDriver(String startTime, String stopTime, String fee) throws Exception {
 
-        for (int i = 0; i < beginDates.length; i++) {
-            Date start = simpleDateTimeFormatter.parse(beginDates[i]);
-            Date stop = simpleDateTimeFormatter.parse(endDates[i]);
-            ArchivedSpot archivedSpot = new ArchivedSpot(registrationNumber, DriverType.VIP, start, stop);
+        final Date[] startStopTimestamps = createStartStopTimestampsByDates(startTime, stopTime);
+        final BigDecimal feeValue = new BigDecimal(fee);
 
-            assertEquals(returnedFee[i], archivedSpot.getFee(currencyRate).get());
-        }
+        ArchivedSpot archivedSpot = new ArchivedSpot("WZE12345", DriverType.VIP, startStopTimestamps[0], startStopTimestamps[1]);
+
+        assertEquals(feeValue, archivedSpot.getFee(currencyRate).get());
+    }
+
+    private Date[] createStartStopTimestamps() {
+        final Calendar calendar = Calendar.getInstance();
+        final Date startDate = calendar.getTime();
+        calendar.add(Calendar.HOUR, 2);
+        final Date stopDate = calendar.getTime();
+
+        return new Date[]{startDate, stopDate};
+    }
+
+    private Date[] createIncorrectStartStopTimeStamps() {
+        final Calendar calendar = Calendar.getInstance();
+        final Date stopDate = calendar.getTime();
+        calendar.add(Calendar.HOUR, 2);
+        final Date startDate = calendar.getTime();
+
+        return new Date[]{startDate, stopDate};
+    }
+
+
+    private Date[] createStartStopTimestampsByDates(String startTime, String stopTime) throws ParseException {
+        final SimpleDateFormat simpleDateTimeFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+        final Date start = simpleDateTimeFormatter.parse(startTime);
+        final Date stop = simpleDateTimeFormatter.parse(stopTime);
+        return new Date[]{start, stop};
     }
 }
 
