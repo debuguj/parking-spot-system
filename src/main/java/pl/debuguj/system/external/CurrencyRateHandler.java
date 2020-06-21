4506037c289c5f073c97761033f2f1c16d3ff167
grze <debuguj@gmail.com>
2020-06-21 12:54:21 +0200

    Add currency rate service.

--- src/main/java/pl/debuguj/system/external/CurrencyRateHandler.java
+++ src/main/java/pl/debuguj/system/external/CurrencyRateHandler.java
@@ -1,9 +1,6 @@
 package pl.debuguj.system.external;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
